d6889da92a78f65ac32c122f615b4ae3840db29a
Soulé Soumaré <sourtoumo@gmail.com>
2026-01-15 21:55:27 +0100

    chore: configure code quality tools (prettier, commitlint, husky)

---
+++ commitlint.config.js
@@ -0,0 +1,25 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'chore',
+        'revert',
+        'ci',
+        'build',
+        'wip',
+      ],
+    ],
+    'subject-case': [0],
+    'body-max-line-length': [0],
+  },
+};

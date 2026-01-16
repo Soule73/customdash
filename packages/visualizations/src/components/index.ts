185457fed416c2f03a0c0d66f3c8a0068dc5fabe
Soulé Soumaré <sourtoumo@gmail.com>
2026-01-16 21:40:24 +0100

    feat(visualizations): add main exports for visualizations package
    - Export all chart, KPI, and table components
    
    - Export types, hooks, and utility functions

---
+++ packages/visualizations/src/components/index.ts
@@ -0,0 +1,3 @@
+export * from './charts';
+export * from './kpi';
+export * from './table';
---
+++ packages/visualizations/src/index.ts
@@ -0,0 +1,5 @@
+export * from './types';
+export * from './types/constants';
+export * from './utils';
+export * from './hooks';
+export * from './components';
